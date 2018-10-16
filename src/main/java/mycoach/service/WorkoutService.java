7a8c33d865c1f248a23e7149d67ba873aab97623
Waby1 <36304863+Waby1@users.noreply.github.com>
2018-10-16 10:20:10 +0200

    version avant exam

--- src/main/java/mycoach/service/WorkoutService.java
+++ src/main/java/mycoach/service/WorkoutService.java
@@ -3,6 +3,7 @@ package mycoach.service;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.transaction.Transactional;
 
 import mycoach.model.ExerciceModel;
 import mycoach.model.WorkoutModel;
@@ -17,7 +18,7 @@ public class WorkoutService  {
 	@Inject
 	ExerciceRepository exerciceRepository ;
 		
-	
+	@Transactional                                           // je securise ma methode 
 	public WorkoutModel save(WorkoutModel workout) {
 		workoutRepository.save(workout);    // ici je persiste le workout en base 
 		
@@ -26,7 +27,7 @@ public class WorkoutService  {
 		
 		} 
 				
-		exerciceRepository.saveAll(workout.getExercices());   // ici je sauvegarde tous les exercices en faisant appel au saveAll de la classe exerciceRepo
+		exerciceRepository.saveAll(workout.getExercices());   // ici je sauvegarde tous les exercices en faisant appel au saveAll de l'interface exerciceRepo
 		
 		return workout;
 	}
